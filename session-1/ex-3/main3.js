a31e2ebb4e0341fd889468f0f2befeda66c32b76
AndreeaNegru <andreeanegru1982@gmail.com>
2025-02-09 14:26:56 +0200

    Completed challange 3

---
+++ session-1/ex-3/main3.js
@@ -0,0 +1,23 @@
+/*CHALLENGE 3: Final Grade Calculation
+A student receives three grades. The program calculates and displays the final average.*/
+
+let firstGrade = prompt('Input first grade');
+firstGrade = Number(firstGrade);
+
+let secondGrade = prompt('Input second grade');
+secondGrade = Number(secondGrade);
+
+let thirdGrade = prompt('Input third grade');
+thirdGrade = Number(thirdGrade);
+
+//alert('The final averige is: ' + (firstGrade + secondGrade + thirdGrade)/3);
+alert(`The final averige is: ${(firstGrade + secondGrade + thirdGrade)/3} `);
+
+
+
+
+
+
+
+
+
